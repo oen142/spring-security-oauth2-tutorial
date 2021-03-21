a1ef48f9fda5112ac031e485f2f57e93a7994856
oen142 <oen142@naver.com>
2021-03-22 00:57:17 +0900

    feat : Api Response 정의

---
+++ src/main/java/com/wani/springsecurityoauth2tutorial/common/response/ApiResponse.java
@@ -0,0 +1,38 @@
+package com.wani.springsecurityoauth2tutorial.common.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ApiResponse<T> {
+
+    private final String status;
+    private final String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-DD HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime serverDateTime;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private final T data;
+
+    private ApiResponse(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.serverDateTime = LocalDateTime.now();
+        this.data = data;
+    }
+
+
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return new ApiResponse<>("SUCCESS", message, data);
+    }
+
+    public static <T> ApiResponse<T> ok(String message) {
+        return ok(message, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>("FAIL", message, null);
+    }
+}
